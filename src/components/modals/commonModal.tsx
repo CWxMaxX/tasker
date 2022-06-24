107cffe4b21ef4c1055fd72b98ba5a2764adc92a
Chamith Wijesooriya <44288824+CWxMaxX@users.noreply.github.com>
2022-06-24 23:41:30 +0530

    Test

---
+++ src/components/modals/commonModal.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
+import "./modal.css";
+
+interface Props {
+	component: ReactJSXElement;
+	title?: string;
+	content: ReactJSXElement;
+}
+
+const style = {
+	position: "absolute" as "absolute",
+	top: "50%",
+	left: "50%",
+	transform: "translate(-50%, -50%)",
+	width: 800,
+	minHeight: 300,
+	backgroundColor: "#fff",
+	borderRadius: "10px",
+	border: "none",
+	outline: "none",
+	padding: "0px 0px 20px 0px ",
+};
+
+export default function CommonModal(props: Props) {
+	const [open, setOpen] = React.useState(false);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
+
+	return (
+		<div>
+			<div onClick={handleOpen}>{props.component}</div>
+			<Modal open={open} onClose={handleClose} sx={{ backdropFilter: "blur(5px)" }} hideBackdrop>
+				<div style={style} className='modal-area'>
+					{props.title && <div className='w-full modalTitle '>{props.title}</div>}
+					<div className='modalContentBox'>{props.content}</div>
+					<div className='w-full flex flex-row justify-end '>
+						<Button sx={{ mr: 3 }} color='error' variant='outlined' onClick={() => setOpen(false)}>
+							Close
+						</Button>
+						<Button variant='contained' sx={{ mr: 3 }}>
+							Submit
+						</Button>
+					</div>
+				</div>
+			</Modal>
+		</div>
+	);
+}
--- src/features/dashboard/home.tsx
+++ src/features/dashboard/home.tsx
@@ -5,6 +5,9 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import DoubleArrowIcon from "@material-ui/icons/DoubleArrow";
 import Button from "@mui/material/Button";
 
+import CommonModal from "../../components/modals/commonModal";
+import AddNewTask from "../../components/modals/dashboardComponents/addNewTask";
+
 // Images
 const addIcon = "/assets/Icons/Add.svg";
 const calenderIcon = "/assets/Icons/Calendar Plus.svg";
@@ -14,100 +17,82 @@ const alarmIcon = "/assets/Icons/Add Reminder.svg";
 const downloadIcon = "/assets/Icons/Download from the Cloud.svg";
 
 const Home: React.FC = () => {
-  const [user, setUser] = useState<User | null>(null);
-  onAuthStateChanged(appAuth, (currentUser) => {
-    setUser(currentUser);
-  });
-  return (
-    <div style={{ height: "100vh", width: "100vw", color: "#fcfcfc" }}>
-      <header className="flex flex-row-reverse w-full h-12 fixed bg-gray-900 items-center pr-5">
-        <span>{user?.displayName ? user.displayName : user?.email}</span>
-        <div className="flex-row flex pr-2">
-          {user?.photoURL ? (
-            <div style={{ borderRadius: "50%", overflow: "clip" }}>
-              <img src={user?.photoURL} alt="User" width={35} />
-            </div>
-          ) : (
-            <AccountCircleIcon
-              sx={{
-                color: `${user ? "#2378ef" : "#f1410a"}`,
-                mr: 1,
-                width: "35px",
-                height: "35px",
-              }}
-              onClick={() => {
-                console.log(user?.uid);
-              }}
-            />
-          )}
-        </div>
-      </header>
-      <div
-        className="flex flex-row"
-        style={{
-          backgroundImage: "url('/assets/Images/backgroundImage.png')",
-          width: "100%",
-          height: "100%",
-          backgroundColor: "#4a148c",
-          color: "#fff",
-        }}
-      >
-        <div className="w-1/12 flex flex-col justify-center items-start sm:pl-5">
-          <div className=" w-16 h-full items-center sm:flex flex-col justify-center hidden fixed">
-            <img
-              src={addIcon}
-              alt="Add"
-              className="mb-8"
-              width={40}
-              onClick={() => {}}
-            />
-            <img
-              src={calenderIcon}
-              alt="Calender"
-              className="mb-8"
-              width={40}
-            />
-            <img
-              src={documentIcon}
-              alt="Document Icon"
-              className="mb-8"
-              width={40}
-            />
-            <img src={locationIcon} alt="Address" className="mb-8" width={40} />
-            <img src={alarmIcon} alt="Alarm" className="mb-8" width={40} />
-            <img
-              src={downloadIcon}
-              alt="Download"
-              className="mb-8"
-              width={40}
-            />
-          </div>
-        </div>
+	const [user, setUser] = useState<User | null>(null);
+	onAuthStateChanged(appAuth, (currentUser) => {
+		setUser(currentUser);
+	});
+	return (
+		<div style={{ height: "100vh", width: "100vw", color: "#fcfcfc" }}>
+			<header className='flex flex-row-reverse w-full h-12 fixed bg-gray-900 items-center pr-5'>
+				<span>{user?.displayName ? user.displayName : user?.email}</span>
+				<div className='flex-row flex pr-2'>
+					{user?.photoURL ? (
+						<div style={{ borderRadius: "50%", overflow: "clip" }}>
+							<img src={user?.photoURL} alt='User' width={35} />
+						</div>
+					) : (
+						<AccountCircleIcon
+							sx={{
+								color: `${user ? "#2378ef" : "#f1410a"}`,
+								mr: 1,
+								width: "35px",
+								height: "35px",
+							}}
+							onClick={() => {
+								console.log(user?.uid);
+							}}
+						/>
+					)}
+				</div>
+			</header>
+			<div
+				className='flex flex-row'
+				style={{
+					backgroundImage: "url('/assets/Images/backgroundImage.png')",
+					width: "100%",
+					height: "100%",
+					backgroundColor: "#4a148c",
+					color: "#fff",
+				}}>
+				<div className='w-1/12 flex flex-col justify-center items-start sm:pl-5'>
+					<div className=' w-16 h-full items-center sm:flex flex-col justify-center hidden fixed'>
+						<CommonModal
+							component={<img src={addIcon} alt='Add' className='mb-8' width={40} />}
+							title='Add New Task ...'
+							content={<AddNewTask />}
+						/>
+						<img src={calenderIcon} alt='Calender' className='mb-8' width={40} />
+						<img src={documentIcon} alt='Document Icon' className='mb-8' width={40} />
+						<img src={locationIcon} alt='Address' className='mb-8' width={40} />
+						<img src={alarmIcon} alt='Alarm' className='mb-8' width={40} />
+						<img src={downloadIcon} alt='Download' className='mb-8' width={40} />
+					</div>
+				</div>
 
-        <div className=" w-8/12 h-full items-center flex">
-          <Button
-            variant="contained"
-            endIcon={<DoubleArrowIcon />}
-            onClick={() => {
-              signOut(appAuth)
-                .then(() => {
-                  window.location.replace("/auth/login");
-                  // Sign-out successful.
-                })
-                .catch((error) => {
-                  // An error happened.
-                  console.log(error);
-                });
-            }}
-          >
-            Log out
-          </Button>
-        </div>
-        <div className=" w-3/12 h-full"></div>
-      </div>
-      <footer className="flex flex-row w-full h-48 bg-gray-900"></footer>
-    </div>
-  );
+				<div className=' w-8/12 h-full items-center flex'></div>
+				<div className=' w-3/12 h-full flex flex-col-reverse pb-10 mr-10 items-end'>
+					<Button
+						sx={{ width: "200px" }}
+						variant='contained'
+						endIcon={<DoubleArrowIcon />}
+						onClick={() => {
+							signOut(appAuth)
+								.then(() => {
+									window.location.replace("/auth/login");
+									// Sign-out successful.
+								})
+								.catch((error) => {
+									// An error happened.
+									console.log(error);
+								});
+						}}>
+						Log out
+					</Button>
+				</div>
+			</div>
+			<footer className='flex flex-row w-full h-48 bg-gray-900'></footer>
+		</div>
+	);
 };
 
 export default Home;
